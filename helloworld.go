5f545436deb9d663c7940153c1b228c2434a4add
SHANMUGAPRIYAP1511 <shanmugapriyap1511@gmail.com>
2022-03-04 00:55:42 +0530

    changed hello

--- helloworld.go
+++ helloworld.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 
-func main(){
+func main() {
 	fmt.Println("Welcome to Go Programming Language")
+	var a string
+	fmt.Println("Enter the Value of A : ")
+	fmt.Scanf("%s",&a)
+	fmt.Println("Value of A : ",a)
 }
